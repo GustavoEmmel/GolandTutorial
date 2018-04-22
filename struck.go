08f41d3d5817eaadb6d6f97114c0f06b12794265
Gustavo Reichelt Emmel <gremmel@gmail.com>
2018-04-22 10:20:35 -0300

    lazy value receiver

--- struck.go
+++ struck.go
@@ -2,6 +2,9 @@ package main
 
 import "fmt"
 
+const usixteenbitmax float64 = 65535
+const kmh_multiple float64 = 1.60934
+
 type car struct {
 	gas_pedal uint16 // min 0 max 65535
 	break_pedal uint16
@@ -9,8 +12,18 @@ type car struct {
 	top_speed_kmh float64
 }
 
+func (c car) kmh() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax)
+}
+
+func (c car) mph() float64 {
+	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax/kmh_multiple)
+}
+
 func main() {
-	a_car := car{22341, 0, 12561, 225.0}
+	a_car := car{65000, 0, 12561, 225.0}
 
 	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
 }

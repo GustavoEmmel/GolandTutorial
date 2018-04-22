3a07b3e211de13746b665d8ccd859dfc7c25b254
Gustavo Reichelt Emmel <gremmel@gmail.com>
2018-04-22 10:35:13 -0300

    fucking new attributes

--- struck.go
+++ struck.go
@@ -20,10 +20,26 @@ func (c car) mph() float64 {
 	return float64(c.gas_pedal) * (c.top_speed_kmh / usixteenbitmax/kmh_multiple)
 }
 
+func (c *car) new_top_speed (newspeed float64) {
+	c.top_speed_kmh = newspeed
+}
+
+func newer_top_speed(c car, speed float64) car {
+	c.top_speed_kmh = speed
+	return c
+}
+
 func main() {
 	a_car := car{65000, 0, 12561, 225.0}
 
 	fmt.Println(a_car.gas_pedal)
+
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+
+	// a_car.new_top_speed(500)
+	a_car = newer_top_speed(a_car, 500) //do the same above
+
 	fmt.Println(a_car.kmh())
 	fmt.Println(a_car.mph())
 }

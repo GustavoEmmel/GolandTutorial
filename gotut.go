50c5dd1778424eb3676a19ccd8907763d581bed5
Gustavo Reichelt Emmel <gustavo.emmel@totvs.com.br>
2018-08-14 14:51:41 -0300

    rabbit poc

--- gotut.go
+++ gotut.go
@@ -1,11 +1,13 @@
 package main
 
-import ("fmt"
-		"math"
-		"math/rand")
+import (
+	"fmt"
+	"math"
+	"math/rand"
+)
 
 func foo() {
-	fmt.Println("The square root of 4 is", math.Sqrt(4))
+	fmt.Println("The square root of 9 is", math.Sqrt(4))
 }
 
 func main() {
